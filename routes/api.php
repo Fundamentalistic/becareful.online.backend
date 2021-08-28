3c77bbd21262763118287aef2dc6b27315cef008
Andrei Bespalov <a.bespalov@alumni.nsu.ru>
2021-08-28 13:44:46 +0700

    api routes was touched

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,11 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('search/words', function(){
+    return response(file_get_contents('../words.json'))
+        ->withHeaders([
+            "Content-Type", "application/json",
+            "Access-Control-Allow-Origin", "*"
+        ]);
+});
